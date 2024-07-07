0e90ece0eead607d715ec693926e10f14ee66248
1Kashyap_K <97424496+1kashyap@users.noreply.github.com>
2024-07-07 20:44:35 +0530

    Update firebase.js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -5,14 +5,14 @@ import { getStorage } from "firebase/storage";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCIIpSmfwinHzn8col2KZJYDqJPZUBQXxk",
-  authDomain: "doc-test-1-c8dab.firebaseapp.com",
-  databaseURL: "https://doc-test-1-c8dab-default-rtdb.firebaseio.com",
-  projectId: "doc-test-1-c8dab",
-  storageBucket: "doc-test-1-c8dab.appspot.com",
-  messagingSenderId: "538079324773",
-  appId: "1:538079324773:web:c0f8b4ffc5ccf67b91f48c",
-  measurementId: "G-Y0B74ZXBMT"
+  apiKey: "**********************************",
+  authDomain: "***************************",
+  databaseURL: "***************************************",
+  projectId: "*************",
+  storageBucket: "**********************",
+  messagingSenderId: "****************",
+  appId: "***************************************",
+  measurementId: "*************"
 };
 
 export const app = initializeApp(firebaseConfig);
